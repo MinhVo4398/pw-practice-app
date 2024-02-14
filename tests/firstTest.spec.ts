4fd0e2e06a1603c3a867bae60a5edc5a54149780
MinhVo <minhvo4398@gmail.com>
2024-02-15 05:38:50 +0700

    add first test, set up playwright

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("The first test", async ({ page }) => {
+  await page.goto("http://localhost:4200/pages/iot-dashboard");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
+});
