e74b24237f5e2e9bac7613b1e340e97f22939bf2
MinhVo <minhvo4398@gmail.com>
2024-02-15 10:53:10 +0700

    user facing locator

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,30 +2,43 @@ import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/pages/iot-dashboard");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
 });
 
-test.describe("Suite 1", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.getByText("Charts").click();
-  });
-  test("The first test", async ({ page }) => {
-    await page.getByText("Form Layouts").click();
-  });
+test("Locator syntax rules", async ({ page }) => {
+  // by Tag name
+  await page.locator("input").first().click();
 
-  test("Navigate to Datepicker page", async ({ page }) => {
-    await page.getByText("Datepicker").click();
-  });
+  // by ID
+  page.locator("#inputEmail1");
+
+  //By Class value
+  page.locator(".shape-rectangle");
+
+  //By attribute
+  page.locator("[placeholder='Email']");
+
+  // Combine different selectors
+  page.locator("input[placeholder='Email'].shape-rectangle");
+
+  //By xpath (NOT RECOMMEND)
+  page.locator("//*[@id='inputEmail']");
+
+  // By partial text match
+  page.locator(":text('Using')");
+
+  // By exact text match
+  page.locator(":text-is('Using the Grid')");
 });
 
-test.describe.skip("Suite 2", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.getByText("Forms").click();
-  });
-  test("The first test", async ({ page }) => {
-    await page.getByText("Form Layouts").click();
-  });
+test("User facing locator", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click();
+  await page.getByRole("button", { name: "Sign in" }).first().click();
 
-  test("Navigate to Datepicker page", async ({ page }) => {
-    await page.getByText("Datepicker").click();
-  });
+  await page.getByLabel("Email").first().click();
+  await page.getByPlaceholder("Jane Doe").click();
+  await page.getByText("Using the Grid").click();
+  // await page.getByTitle("IoT Dashboard").click();
+  await page.getByTestId("SignIn").click();
 });
