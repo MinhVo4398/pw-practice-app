238601b5e2f34738c2a3bdae961bc5f6d98aafbf
MinhVo <minhvo4398@gmail.com>
2024-02-15 13:15:33 +0700

    reusing locator

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -51,11 +51,58 @@ test.only("Locating child elements", async ({ page }) => {
     .locator(":text-is('Option 2')")
     .click();
 
-  await page.locator("nb-card").getByRole("button", { name: "Sign in" }).first().click();
+  await page
+    .locator("nb-card")
+    .getByRole("button", { name: "Sign in" })
+    .first()
+    .click();
   await page.locator("nb-card").nth(3).getByRole("button").click();
 });
 
+test("Locating parent element", async ({ page }) => {
+  await page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  await page
+    .locator("nb-card", { has: page.locator("#inputEmail1") })
+    .getByRole("textbox", { name: "Email" })
+    .click();
 
-test('Locating parent element', async ({ page }) => {
-    
-})
+  await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic Form" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator(".status-danger") })
+    .getByRole("textbox", { name: "Password" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator("nb-checkbox") })
+    .filter({ hasText: "Sign in" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+});
+
+test("Reusing locator", async ({ page }) => {
+  const basicForm = await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic Form" });
+  const emailField = basicForm.getByRole("textbox", { name: "Email" });
+
+  await emailField.fill("test@test.com");
+  await basicForm
+    .filter({ hasText: "Basic Form" })
+    .getByRole("textbox", { name: "Password" })
+    .fill("Welcome123");
+  await basicForm.locator("nb-checkbox").click();
+
+  await basicForm.filter({ hasText: "Basic Form" }).getByRole("button").click();
+
+  await expect(emailField).toHaveValue("test@test.com");
+});
