17231f9665d4dfa90e61d1c5daaeb81e5ee1a154
MinhVo <minhvo4398@gmail.com>
2024-02-15 12:46:59 +0700

    handle child element

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -42,3 +42,20 @@ test("User facing locator", async ({ page }) => {
   // await page.getByTitle("IoT Dashboard").click();
   await page.getByTestId("SignIn").click();
 });
+
+test.only("Locating child elements", async ({ page }) => {
+  await page.locator("nb-card nb-radio :text-is('Option 1')").click();
+  await page
+    .locator("nb-card")
+    .locator("nb-radio")
+    .locator(":text-is('Option 2')")
+    .click();
+
+  await page.locator("nb-card").getByRole("button", { name: "Sign in" }).first().click();
+  await page.locator("nb-card").nth(3).getByRole("button").click();
+});
+
+
+test('Locating parent element', async ({ page }) => {
+    
+})
