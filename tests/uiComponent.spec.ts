1a531b1dd484ebc8aa468d8480c8f3f84314abbf
minhvo <minhvo4398@gmail.com>
2024-02-16 16:21:52 +0700

    fill out value input field

--- tests/uiComponent.spec.ts
+++ tests/uiComponent.spec.ts
@@ -4,9 +4,24 @@ test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/pages/iot-dashboard");
 });
 
-test("Form layout pages", async ({ page }) => {
-  test.beforeEach(async ({ page }) => {
-    await page.getByText("Forms").click();
-    await page.getByText("Form Layouts").click();
-  });
-});
+test.describe('Form layout page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText("Forms").click();
+        await page.getByText("Form Layouts").click(); 
+    })
+    test('Input fields', async ({ page }) => {
+        const usingtheGridEmailInput  = page.locator("nb-card",{hasText:"Using the Grid"}).getByRole("textbox",{name:"Email"})
+
+        await usingtheGridEmailInput.fill("test@test.com");
+        
+        //* Generic assertion
+        const inputValue = await usingtheGridEmailInput.inputValue(); // take value from input field
+        expect(inputValue).toEqual("test@test.com");
+
+        //* Locator assertion
+        await expect(usingtheGridEmailInput).toHaveValue("test@test.com");
+    })
+    
+})
+
+
