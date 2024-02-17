3c0092a6b33197b4560d4656fa460ca52dda1086
minhvo <minhvo4398@gmail.com>
2024-02-17 14:45:05 +0700

    navigation page refractor code

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,15 +1,46 @@
-
 import { Page, Locator } from "@playwright/test";
 
-export  class NavigationPage {
-    readonly  page: Page;
+export class NavigationPage {
+  readonly page: Page;
 
-    constructor(page: Page) {
-        this.page = page;
-    }
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    async formLayoutsPage() {
-    await this.page.getByText("Forms").click();
+  async formLayoutsPage() {
+    await this.selectGroupMenuItem("Forms");
     await this.page.getByText("Form Layouts").click();
+  }
+
+  async datePickerDate() {
+    await this.selectGroupMenuItem("Forms");
+    await this.page.getByText("Datepicker").click();
+  }
+
+  async smartTablePage() {
+    await this.selectGroupMenuItem("Tables & Data");
+    await this.page.getByText("Smart Table").click();
+  }
+
+  async toastrPage() {
+    await this.selectGroupMenuItem("Modal & Overlays"); 
+    await this.page.getByText("Toastr").click();
+  }
+
+  async tooltipPage() {
+    await this.selectGroupMenuItem("Modal & Overlays"); 
+    await this.page.getByText("Tooltip").click();
+  }
+  /**
+   * This method is to check the the groupItemTitle (Ex: Forms) is collapse or not
+   * @param groupItemTitle
+   */
+  private async selectGroupMenuItem(groupItemTitle: string) {
+    const groupMenuItem = this.page.getByTitle(groupItemTitle);
+    const expandedState = await groupMenuItem.getAttribute("aria-expanded");
+    if (expandedState == "false") {
+      // if false means collapse
+      await groupMenuItem.click();
     }
+  }
 }
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,12 +1,15 @@
-import {test, expect } from '@playwright/test';
-import {NavigationPage} from '../page-objects/navigationPage';
+import { test, expect } from "@playwright/test";
+import { NavigationPage } from "../page-objects/navigationPage";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:4200/pages/iot-dashboard");
-  });
-  
-  test('Navigate to form page', async ({ page }) => {
-    const navigationPage = new NavigationPage(page);
-   await navigationPage.formLayoutsPage();
-  })
-  
+  await page.goto("http://localhost:4200/pages/iot-dashboard");
+});
+
+test("Navigate to form page", async ({ page }) => {
+  const navigationPage = new NavigationPage(page);
+  await navigationPage.formLayoutsPage();
+  await navigationPage.datePickerDate();
+  await navigationPage.smartTablePage();
+  await navigationPage.toastrPage();
+  await navigationPage.tooltipPage();
+}); 
