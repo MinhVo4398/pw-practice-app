17c8d2732fa662077cf0a225e4f92e1cf09551fb
minhvo <minhvo4398@gmail.com>
2024-02-18 09:52:28 +0700

    create page manager

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,39 +1,36 @@
 import { test, expect } from "@playwright/test";
-import { NavigationPage } from "../page-objects/navigationPage";
-import { FormLayoutsPage } from "../page-objects/formLayoutPage";
-import { DatepickerPage } from "../page-objects/datepickerPage";
+import { PageManager } from "../page-objects/pageManager";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/pages/iot-dashboard");
 });
 
 test("Navigate to form page", async ({ page }) => {
-  const navigationPage = new NavigationPage(page);
-  await navigationPage.formLayoutsPage();
-  await navigationPage.datePickerDate();
-  await navigationPage.smartTablePage();
-  await navigationPage.toastrPage();
-  await navigationPage.tooltipPage();
+  const pm = new PageManager(page);
+
+  await pm.navigateTo().formLayoutsPage();
+  await pm.navigateTo().datePickerDate();
+  await pm.navigateTo().smartTablePage();
+  await pm.navigateTo().toastrPage();
+  await pm.navigateTo().tooltipPage();
 });
 
 test.only("Parametrized methods", async ({ page }) => {
-  const navigationPage = new NavigationPage(page);
-  const onFormLayoutsPage = new FormLayoutsPage(page);
-  const onDatepickerPage = new DatepickerPage(page);
+  const pm = new PageManager(page);
 
-  await navigationPage.formLayoutsPage();
-  await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption(
-    "test@test.com",
-    "welcome1",
-    "Option 1"
-  );
-  await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(
-    "David",
-    "test@test.com",
-    false
-  );
-  await navigationPage.datePickerDate();
-  await onDatepickerPage.selectCommonDatePickerDateFromToday(10);
-  await onDatepickerPage.selectDatepickerWithRangeFromToday(6,15);
+  await pm.navigateTo().formLayoutsPage();
+  await pm
+    .onFormLayoutsPage()
+    .submitUsingTheGridFormWithCredentialsAndSelectOption(
+      "test@test.com",
+      "welcome1",
+      "Option 1"
+    );
+  await pm
+    .onFormLayoutsPage()
+    .submitInlineFormWithNameEmailAndCheckbox("David", "test@test.com", false);
+  await pm.navigateTo().datePickerDate();
+  await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(9);
+  await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10);
   await page.pause();
 });
