1d4fbd5414815224a299dcf41830fc8b20ce88d5
minhvo <minhvo4398@gmail.com>
2024-02-20 04:11:52 +0700

    add retries

--- tests/uiComponent.spec.ts
+++ tests/uiComponent.spec.ts
@@ -1,30 +1,37 @@
 import { test, expect } from "@playwright/test";
+import { delay } from 'rxjs/operators';
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/pages/iot-dashboard");
 });
 
-test.describe("Form layout page", () => {
+test.describe.only("Form layout page", () => {
+  test.describe.configure({retries:2})
+
   test.beforeEach(async ({ page }) => {
     await page.getByText("Forms").click();
     await page.getByText("Form Layouts").click();
   });
-  test("Input fields", async ({ page }) => {
-    const usingtheGridEmailInput = page
+  test.only("Input fields", async ({ page }, testInfo) => {
+    if(testInfo.retry) {
+      // do something
+    }
+
+    const usingTheGridEmailInput = page
       .locator("nb-card", { hasText: "Using the Grid" })
       .getByRole("textbox", { name: "Email" });
 
-    await usingtheGridEmailInput.fill("test@test.com");
+    await usingTheGridEmailInput.fill("tes2@test.com");
 
     //* Generic assertion
-    const inputValue = await usingtheGridEmailInput.inputValue(); // take value from input field
+    const inputValue = await usingTheGridEmailInput.inputValue(); // take value from input field
     expect(inputValue).toEqual("test@test.com");
 
     //* Locator assertion
-    await expect(usingtheGridEmailInput).toHaveValue("test@test.com");
+    await expect(usingTheGridEmailInput).toHaveValue("test@test.com");
   });
 
-  test("Radio button", async ({ page }) => {
+  test.only("Radio button", async ({ page }) => {
     const usingtheGridForm = page.locator("nb-card", {
       hasText: "Using the Grid",
     });
