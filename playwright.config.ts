9276b077d2694aa606497f152145a469d41196a1
minhvo <minhvo4398@gmail.com>
2024-02-25 08:45:33 +0700

    add allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 import type { TestOptions } from "./test-option";
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 export default defineConfig<TestOptions>({
   retries: 1,
@@ -10,14 +9,15 @@ export default defineConfig<TestOptions>({
   globalTimeout: 60000,
   testDir: "./tests",
   fullyParallel: false,
-  reporter: "html",
-
+  reporter: [["allure-playwright"], ["html"]],
   use: {
     baseURL:
-      process.env.DEV === "1" ? "http://localhost:4201/" : 
-      process.env.STAGING == "1" ? "http://localhost:4202/"
-      : "http://localhost:4200/",  
-    globalsQaUrl: 'https://www.globalsqa.com/demo-site/draganddrop/',
+      process.env.DEV === "1"
+        ? "http://localhost:4201/"
+        : process.env.STAGING == "1"
+        ? "http://localhost:4202/"
+        : "http://localhost:4200/",
+    globalsQaUrl: "https://www.globalsqa.com/demo-site/draganddrop/",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
     //actionTimeout: 20000,
@@ -43,21 +43,21 @@ export default defineConfig<TestOptions>({
 
     {
       name: "firefox",
-      use: {browserName: 'firefox' },
+      use: { browserName: "firefox" },
     },
     {
-      name: 'pageObjectFullScreen',
-      testMatch:'usePageObjects.spec.ts',
+      name: "pageObjectFullScreen",
+      testMatch: "usePageObjects.spec.ts",
       use: {
-        viewport: {width: 1920, height: 1080}
-      }
+        viewport: { width: 1920, height: 1080 },
+      },
     },
     {
-        name:'mobile',
-        testMatch:'testMobile.spec.ts',
-        use: {
-          ...devices['iPhone 13 Pro'],
-        }
-    }
+      name: "mobile",
+      testMatch: "testMobile.spec.ts",
+      use: {
+        ...devices["iPhone 13 Pro"],
+      },
+    },
   ],
 });
